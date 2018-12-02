c0881fa60bb0705a28f9158743efe83c9f21d6c2
Meg4R0M <psykoterro@gmail.com>
2018-12-02 13:55:54 +0100

    Update phpmemadmin and change vhost to point /home/site/www

--- 7.2/conf/bootstrap.sh
+++ 7.2/conf/bootstrap.sh
@@ -6,7 +6,7 @@ if [ "$FIX_OWNERSHIP" != "" ]; then
 fi
 
 # display logs
-tail -F /var/log/apache2/sites/error.log &
+#tail -F /var/log/apache2/sites/error.log &
 
 echo "[i] Starting daemon..."
 # run apache httpd daemon
